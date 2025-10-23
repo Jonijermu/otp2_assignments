2fd04d6e88a198e9dc75e965469603d6a329e74d
joni.heikkila <joni.heikkila@metropolia.fi>
2025-10-23 14:40:19 +0300

    Update pom.xml and TestMain.java for JUnit 4 compatibility and Java version upgrade

--- assignment_1/src/test/java/TestMain.java
+++ assignment_1/src/test/java/TestMain.java
@@ -1,9 +1,12 @@
-import org.junit.jupiter.api.Test;
+
+
+import org.junit.Test;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 public class TestMain {
 

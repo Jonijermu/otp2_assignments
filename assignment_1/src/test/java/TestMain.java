5bfe4004e5d9d2c9b236e72f4efb8c5c466dc0a1
jonijermu <joni.heikkila@metropolia.fi>
2025-10-24 23:51:54 +0300

    Refactor Dockerfile and Jenkinsfile for improved build process; update pom.xml for JUnit 5 compatibility

--- assignment_1/src/test/java/TestMain.java
+++ assignment_1/src/test/java/TestMain.java
@@ -1,11 +1,7 @@
-
-
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import java.util.Locale;
 import java.util.ResourceBundle;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestMain {
@@ -22,7 +18,7 @@ public class TestMain {
 
     @Test
     public void testFinnishBundle() {
-        Locale locale = new Locale("fi", "UFI");
+        Locale locale = new Locale("fi", "FI");
         ResourceBundle rb = ResourceBundle.getBundle("languages", locale);
         String message = rb.getString("b");
         assertEquals("Syötä tuotteen hinta:", message);

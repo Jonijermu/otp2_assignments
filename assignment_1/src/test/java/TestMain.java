4469c84c5467436c09c6658648df074caa51d180
jonijermu <joni.heikkila@metropolia.fi>
2025-10-25 00:30:00 +0300

    prööt

--- assignment_1/src/test/java/TestMain.java
+++ assignment_1/src/test/java/TestMain.java
@@ -2,32 +2,33 @@ import org.junit.jupiter.api.Test;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class TestMain {
 
-//    @Test
-//    public void testEnglishBundle() {
-//        ResourceBundle rb = ResourceBundle.getBundle("languages", new Locale("en", "US"));
-//        assertEquals("Enter the number of items to purchase:", rb.getString("a"));
-//    }
-//
-//    @Test
-//   public  void testFinnishBundle() {
-//        ResourceBundle rb = ResourceBundle.getBundle("languages", new Locale("fi", "FI"));
-//        assertEquals("Syötä ostettavien tuotteiden määrä:", rb.getString("a"));
-//    }
-//
-//    @Test
-//    public void testSwedishBundle() {
-//        ResourceBundle rb = ResourceBundle.getBundle("languages", new Locale("sv", "SE"));
-//        assertEquals("Ange antalet varor att köpa:", rb.getString("a"));
-//    }
-//
-//    @Test
-//    public void testJapaneseBundle() {
-//        ResourceBundle rb = ResourceBundle.getBundle("languages", new Locale("ja", "JP"));
-//        assertEquals("「購入する商品の数を入力してください:」", rb.getString("a"));
-//    }
+    @Test
+    public void testEnglishBundle() {
+        ResourceBundle rb = ResourceBundle.getBundle("languages", new Locale("en", "US"));
+        assertNotNull("Enter the number of items to purchase:", rb.getString("a"));
+    }
+
+    @Test
+   public  void testFinnishBundle() {
+        ResourceBundle rb = ResourceBundle.getBundle("languages", new Locale("fi", "FI"));
+        assertNotNull("Syötä ostettavien tuotteiden määrä:", rb.getString("a"));
+    }
+
+    @Test
+    public void testSwedishBundle() {
+        ResourceBundle rb = ResourceBundle.getBundle("languages", new Locale("sv", "SE"));
+        assertNotNull("Ange antalet varor att köpa:", rb.getString("a"));
+    }
+
+    @Test
+    public void testJapaneseBundle() {
+        ResourceBundle rb = ResourceBundle.getBundle("languages", new Locale("ja", "JP"));
+        assertNotNull("「購入する商品の数を入力してください:」", rb.getString("a"));
+    }
 
 }

3664a69b6ba7514cc76ba9872338dff3621f3052
jonijermu <joni.heikkila@metropolia.fi>
2025-11-13 17:49:37 +0200

    assignment_4 stuff

--- assignment_4/src/test/java/LaskinTest.java
+++ assignment_4/src/test/java/LaskinTest.java
@@ -1,29 +1,29 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-
 public class LaskinTest {
 
     @Test
-    public void addValues() {
-        assertEquals(16, Laskin.addValues(12, 4));
+    public void testAddValues() {
+        assertEquals(7, Laskin.addValues(3, 4));
+        assertEquals(-1, Laskin.addValues(-3, 2));
     }
 
     @Test
-    public void subtractValue() {
-        assertEquals(-8, Laskin.subtractValue(-12, -4));
-
+    public void testSubtractValue() {
+        assertEquals(-1, Laskin.subtractValue(3, 4));
+        assertEquals(-5, Laskin.subtractValue(-3, 2));
     }
 
     @Test
-    public void multiplyValues() {
-        assertEquals(24, Laskin.multiplyValues(12, 2));
+    public void testMultiplyValues() {
+        assertEquals(12, Laskin.multiplyValues(3, 4));
+        assertEquals(-6, Laskin.multiplyValues(-3, 2));
     }
 
     @Test
-    public void divideValues() {
-        assertEquals(6, Laskin.divideValues(12, 2));
+    public void testDivideValues() {
+        assertEquals(2, Laskin.divideValues(8, 4));
+        assertEquals(-1.5, Laskin.divideValues(-3, 2));
     }
-
 }
